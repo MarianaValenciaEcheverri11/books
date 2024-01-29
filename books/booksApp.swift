db10e5c57050adfa4277233ed41d2a7a6939a83b
MarianaValenciaEcheverri11 <marianavalencia161017@gmail.com>
2024-01-29 14:09:34 -0500

    Initial Commit

---
+++ books/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by Mariana Valencia Echeverri on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
